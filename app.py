a2c05187e69f31f2884e11bdaf72fe5164368b1d
Hans Hazairi <hanshazairi@gmail.com>
2021-03-27 15:25:15 +0800

    Added 'joke' command

--- app.py
+++ app.py
@@ -1,7 +1,33 @@
-from flask import Flask
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
+import requests
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-	return 'Hello World!'
+@app.route('/', methods=['POST'])
+def bot():
+	incoming_msg = request.values.get('Body', '').lower()
+	resp = MessagingResponse()
+	msg = resp.message()
+	responded = False
+
+	if 'joke' in incoming_msg:
+		r = requests.get('https://official-joke-api.appspot.com/random_joke')
+
+		if r.status_code == 200:
+			data = r.json()
+			joke = f'{data["setup"]}\n\n{data["punchline"]}'
+
+		else:
+			joke = f'{r.status_code} error occurred retrieving joke.'
+
+		msg.body(joke)
+		responded = True
+
+	if not responded:
+		msg.body('I do not recognise that command.')
+
+	return str(resp)
+
+#if __name__ == '__main__':
+#	app.run()
