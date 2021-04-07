865375505649b01a616bbd2bbcc933715030b38f
Hans Hazairi <hanshazairi@gmail.com>
2021-04-07 21:58:03 +0800

    Added replies
    Added replies for '2', '21', '22', '23', '24', '25', '26' & '27'

--- main.py
+++ main.py
@@ -10,13 +10,37 @@ client = Client(os.environ['SID'], os.environ['TOKEN'])
 
 @app.route('/', methods = ['POST'])
 def bot():
-	text = request.values.get('Body', '')
+	text = request.values.get('Body', '').lower()
 	
 	if text == 'joke':	
 		body = f.get_joke()
 
-	elif text == '1' or text.lower() == 'latest':
+	elif text == '1' or text == '🔢' or text == 'latest':
 		body = r.reply_1_latest
+
+	elif text == '2' or text == '💉' or text == 'vaccine':
+		body = r.reply_2_vaccine
+
+	elif text == '21':
+		body = r.reply_21
+
+	elif text == '22':
+		body = r.reply_22
+
+	elif text == '23':
+		body = r.reply_23
+
+	elif text == '24':
+		body = r.reply_24
+
+	elif text == '25':
+		body = r.reply_25
+
+	elif text == '26':
+		body = r.reply_26
+
+	elif text == '27':
+		body = r.reply_27
 
 	else:
 		body = r.start
